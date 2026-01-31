e9e3059483fdf76f47790922de82e51ec2190814
JuaniBattelli <juan.battelli@gmail.com>
2026-01-31 13:17:19 -0300

    Separe en escenas la parte de cliente y comence animaciones

--- scripts/client_window.gd
+++ scripts/client_window.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var cw_next_window_button: Button
 @export var cw_dialogue_bubble: Sprite2D
 
+
 ##Vas a tener que iterar sobre los sprites y asignarlos con el indice. Lo mismo con el pedido
 ## Que puede ser un label 
 ## Y en algun script commons asocias pedido y mascara. 
@@ -24,5 +25,5 @@ func spawn_client():
 func get_random_client():
 	var random_sprite = randi()%10
 	var random_order = randi()%10
-	var choosen_sprite = C.CLIENT_SPRITES[random_sprite]
-	var choosen_orden = C.CLIENT_DIALOGUES[random_order]
+	#var choosen_sprite = C.CLIENT_SPRITES[random_sprite]
+	#var choosen_orden = C.CLIENT_DIALOGUES[random_order]
