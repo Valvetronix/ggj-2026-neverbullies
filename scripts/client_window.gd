c7e75d43e352def129817dc797e34fc2d5d6b388
Julen Leizza <julenneakaill@gmail.com>
2026-01-31 20:48:36 -0300

    los clientes aparecen en la misma posicion que el marker2d

--- scripts/client_window.gd
+++ scripts/client_window.gd
@@ -1,12 +1,10 @@
 extends LevelState
-@export var cw_background: Sprite2D
-@export var cw_client: Sprite2D
-@export var cw_next_window_button: Button 
-@export var cw_dialogue_bubble: Sprite2D
+@export var client: Sprite2D
+@export var client_marker: Marker2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$CWClient.global_position = client_marker.global_position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
